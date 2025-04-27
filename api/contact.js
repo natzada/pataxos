54299e0fadabff3c69222b1c2be8166473f82765
natzada <nathalia.yoshioka.dev2024@gmail.com>
2025-04-27 01:39:44 -0300

    Create contact.js

---
+++ api/contact.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+module.exports = async (req, res) => {
+  const { email, message } = req.body;
+
+  // Configuração do transporte de e-mail (Gmail)
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+  });
+
+  // Configuração do e-mail
+  const mailOptions = {
+    from: email,
+    to: 'seu-email@gmail.com', // Seu email
+    subject: 'Nova Mensagem do Formulário de Contato',
+    text: message,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: 'E-mail enviado com sucesso!' });
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao enviar o e-mail' });
+  }
+};
